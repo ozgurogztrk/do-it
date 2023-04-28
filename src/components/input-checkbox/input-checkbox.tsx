9e41f78b00d98eeffeb3357ac637d79de64ade4f
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-04-28 21:07:51 +0300

    Bugfix indicator placement

--- src/components/input-checkbox/input-checkbox.tsx
+++ src/components/input-checkbox/input-checkbox.tsx
@@ -1,3 +1,4 @@
+import { Icon } from "@iconify/react";
 import { useState } from "react";
 import styles from "./input-checkbox.module.scss";
 
@@ -6,9 +7,20 @@ export default function InputCheckbox({
   defaultChecked = false,
   children,
 }: InputCheckboxProps) {
+  const [checkedState, setCheckedState] = useState(defaultChecked);
+
+  const handleChange = (event: any) => {
+    setCheckedState(event.target.checked);
+  };
   return (
     <label className={styles.wrapper}>
-      <input type="checkbox" name={name} defaultChecked={defaultChecked} />
+      <input
+        type="checkbox"
+        name={name}
+        defaultChecked={defaultChecked}
+        onChange={handleChange}
+      />
+      {checkedState ? <Icon icon={"lucide:check"} /> : null}
       <span className={styles.checkbox} />
       {children}
     </label>
