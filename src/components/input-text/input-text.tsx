43125670446ad97ebbee1958670a7b88ae0a2856
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-05-04 20:30:24 +0300

    Change type name

--- src/components/input-text/input-text.tsx
+++ src/components/input-text/input-text.tsx
@@ -5,7 +5,7 @@ export default function InputText({
   defaultValue = "",
   placeholder = "Placeholder...",
   hasIcon = false,
-}: InputProps) {
+}: InputTextProps) {
   return (
     <input
       type="text"
--- src/components/input-text/types.d.ts
+++ src/components/input-text/types.d.ts
@@ -1,4 +1,4 @@
-type InputProps = {
+type InputTextProps = {
   name?: string;
   defaultValue?: string;
   placeholder?: string;
