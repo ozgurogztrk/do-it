d371cb46917add3994e8dd07b636d39e1c87f1f1
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-05-04 19:48:05 +0300

    Add firebase

---
+++ src/utils/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJLqn45nh8EsCU5-2Be1fLNc2qZVqrVmw",
+  authDomain: "react-todo-1f2ec.firebaseapp.com",
+  projectId: "react-todo-1f2ec",
+  storageBucket: "react-todo-1f2ec.appspot.com",
+  messagingSenderId: "223716289488",
+  appId: "1:223716289488:web:071188b0be84f9d64727d5",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
