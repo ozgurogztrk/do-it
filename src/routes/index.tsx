767d3d58485a3b767029bd2ed1428f3129504da0
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-05-13 00:21:14 +0300

    Remove routes file

--- src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import Favorites from "src/pages/favorites";
-import Index from "src/pages";
-import List from "src/pages/id";
-
-export const routes = [
-  { id: 0, path: "/", element: <Index /> },
-  { id: 1, path: "/favorites", element: <Favorites /> },
-  { id: 2, path: "/:id", element: <List /> },
-];
