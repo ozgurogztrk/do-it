52cbf54f424b0ad53c17235892ff0363e6081476
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-05-13 00:23:19 +0300

    Make routes protected

--- src/features/animated-routes/animated-routes.tsx
+++ src/features/animated-routes/animated-routes.tsx
@@ -1,6 +1,12 @@
 import { Routes, useLocation, Route } from "react-router-dom";
-import { routes } from "src/routes";
 import { AnimatePresence } from "framer-motion";
+import ProtectedRoute from "src/components/protected-route";
+import DefaultLayout from "src/layouts/default-layout";
+import Index from "src/pages";
+import List from "src/pages/id";
+import Favorites from "src/pages/favorites";
+import SignIn from "src/pages/sign-in";
+import SignUp from "src/pages/sign-up";
 
 export default function AnimatedRoutes() {
   const location = useLocation();
@@ -9,9 +15,16 @@ export default function AnimatedRoutes() {
     <>
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
-          {routes.map(({ id, path, element }: any) => (
-            <Route key={id} path={path} element={element} />
-          ))}
+          <Route element={<ProtectedRoute />}>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<Index />} />
+              <Route path=":id" element={<List />} />
+              <Route path="favorites" element={<Favorites />} />
+            </Route>
+          </Route>
+
+          <Route path="sign-in" element={<SignIn />} />
+          <Route path="sign-up" element={<SignUp />} />
         </Routes>
       </AnimatePresence>
     </>
