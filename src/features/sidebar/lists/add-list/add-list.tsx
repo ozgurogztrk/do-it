1d15583aa1481ca44ef4701c3e844a72ac210daf
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-05-22 23:37:07 +0300

    Revize feature to add list to cloud firestore

--- src/features/sidebar/lists/add-list/add-list.tsx
+++ src/features/sidebar/lists/add-list/add-list.tsx
@@ -1,4 +1,6 @@
 import { useContext, useState } from "react";
+import { db } from "src/utils/firebase-config";
+import { updateDoc, doc } from "firebase/firestore";
 import { ListsContext } from "src/contexts/lists-context";
 import { notContains } from "src/utils/not-contains";
 import ButtonIcon from "src/components/button-icon";
@@ -6,16 +8,20 @@ import InputText from "src/components/input-text";
 import styles from "./add-list.module.scss";
 
 export default function AddList() {
-  const { lists, setLists } = useContext(ListsContext);
+  const { lists } = useContext(ListsContext);
   const [listTitle, setListTitle] = useState("");
 
-  const addNewList = (event: any) => {
+  const docRef = doc(db, "list-collection", "1wSSriX8Y6ism0UyzTJP");
+
+  const addNewList = async (event: any) => {
     event.preventDefault();
 
     if (notContains(lists, listTitle)) {
-      setLists([...lists, { id: lists.length, title: listTitle, todos: [] }]);
+      await updateDoc(docRef, {
+        lists: [...lists, { id: lists.length, title: listTitle, todos: [] }],
+      });
 
-      event.target.reset();
+      setListTitle("");
     }
   };
 
@@ -26,6 +32,7 @@ export default function AddList() {
         placeholder="Add New List"
         hasIcon={true}
         onChange={(event) => setListTitle(event.target.value)}
+        value={listTitle}
       />
     </form>
   );
