52fd2c4674d61e100e206bfef560e06bcb586b98
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-05-22 23:40:11 +0300

    Redid lists-context for getting data from firestore

--- src/contexts/lists-context.tsx
+++ src/contexts/lists-context.tsx
@@ -1,26 +1,26 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { db } from "src/utils/firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 export const ListsContext = createContext({} as any);
 
 export default function ListsContextProvider({ children }: any) {
-  const [lists, setLists] = useState([
-    {
-      id: 0,
-      title: "List 1",
-      todos: [
-        { id: 0, title: "Go to mall", isFavorite: true },
-        { id: 1, title: "Dentist appointment", isFavorite: false },
-      ],
-    },
-    {
-      id: 1,
-      title: "List 2",
-      todos: [
-        { id: 0, title: "Make amogus memes", isFavorite: false },
-        { id: 1, title: "Sleep well", isFavorite: true },
-      ],
-    },
-  ]);
+  const [lists, setLists] = useState<any>([]);
+
+  const fetchListCollection = async () => {
+    await getDocs(collection(db, "list-collection")).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({
+        lists: doc.get("lists"),
+        id: doc.id,
+      }));
+
+      setLists(newData[0].lists);
+    });
+  };
+
+  useEffect(() => {
+    fetchListCollection();
+  }, [lists]);
 
   return (
     <ListsContext.Provider value={{ lists, setLists }}>
