ab8227cbb5973031e2220e2fdb92960001c42095
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-05-27 21:27:17 +0300

    Remove scss of settings button

--- src/features/sidebar/user-actions/settings/settings.tsx
+++ src/features/sidebar/user-actions/settings/settings.tsx
@@ -1,12 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import ButtonIcon from "src/components/button-icon/button-icon";
-import styles from "./settings.module.scss";
 
 export default function Settings() {
   const navigate = useNavigate();
 
   const toSettings = () => {
-    navigate("/");
+    navigate("/settings");
   };
 
   return <ButtonIcon icon="lucide:settings" onClick={toSettings} />;
