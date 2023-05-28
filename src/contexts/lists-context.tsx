9b009556edfff224b9530e5e0399c193a9354165
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-05-28 20:13:10 +0300

    Bugfix loop of document read from firestore

--- src/contexts/lists-context.tsx
+++ src/contexts/lists-context.tsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { db } from "src/utils/firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, onSnapshot, query } from "firebase/firestore";
 
 // Create a lists context
 export const ListsContext = createContext({} as any);
@@ -11,21 +11,28 @@ export default function ListsContextProvider({ children }: any) {
   const [lists, setLists] = useState<any>([]);
 
   // Fetch lists from cloud firestore and add that data to lists variable using setLists
-  const fetchListCollection = async () => {
-    await getDocs(collection(db, "list-collection")).then((querySnapshot) => {
-      const newData = querySnapshot.docs.map((doc) => ({
+  const fetchListCollection = () => {
+    const collectionRef = collection(db, "list-collection");
+    const queryRef: any = query(collectionRef);
+
+    const unsubscribe = onSnapshot(queryRef, (querySnapshot: any) => {
+      const newData = querySnapshot.docs.map((doc: any) => ({
         lists: doc.get("lists"),
         id: doc.id,
       }));
-
       setLists(newData[0].lists);
     });
+
+    return unsubscribe;
   };
 
   // Check if there are any changes to the lists
   useEffect(() => {
-    fetchListCollection();
-  }, [lists]);
+    const unsubscribe = fetchListCollection();
+
+    // Clean up the listener when the component is unmounted
+    return () => unsubscribe();
+  }, []);
 
   return (
     <ListsContext.Provider value={{ lists, setLists }}>
