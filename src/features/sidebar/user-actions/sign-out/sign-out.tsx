f0ea72da06a5beb8691a9be61a060daecd6c28f7
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-05-29 15:36:05 +0300

    Remove navigate to sign in page after signing out

--- src/features/sidebar/user-actions/sign-out/sign-out.tsx
+++ src/features/sidebar/user-actions/sign-out/sign-out.tsx
@@ -1,19 +1,12 @@
-import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "src/utils/firebase-config";
 import ButtonIcon from "src/components/button-icon/button-icon";
 
 export default function SignOut() {
-  const navigate = useNavigate();
-
   const handleSignOut = () => {
-    signOut(auth)
-      .then(() => {
-        navigate("/sign-in");
-      })
-      .catch((error) => {
-        console.error(error.code, error.message);
-      });
+    signOut(auth).catch((error) => {
+      console.error(error.code, error.message);
+    });
   };
 
   return <ButtonIcon icon="lucide:log-out" onClick={handleSignOut} />;
