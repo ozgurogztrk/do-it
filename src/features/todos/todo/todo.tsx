9a49a48a03de9b83d8f75219e29068ced9574d05
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-05-31 02:19:48 +0300

    Add completion checkbox

--- src/features/todos/todo/todo.tsx
+++ src/features/todos/todo/todo.tsx
@@ -1,5 +1,8 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { onSnapshot, updateDoc } from "firebase/firestore";
 import { ListsContext } from "src/contexts/lists-context";
+import InputCheckbox from "src/components/input-checkbox";
 import styles from "./todo.module.scss";
 
 export default function Todo({
@@ -9,26 +12,70 @@ export default function Todo({
   setSelectedTodo,
   setFavoriteTodosId = () => 0,
 }: TodoProps) {
-  // Get lists variable from lists context
-  const { lists } = useContext(ListsContext);
-
-  // The function of toggling the todo details menu
-  const toggleTodoDetails = () => {
-    setIsDetailsOpen(true);
-    setSelectedTodo(getTodo);
-    setFavoriteTodosId(id);
-  };
+  const { lists, userDocRef } = useContext(ListsContext);
 
   // Get the information of selected todo
   const getTodo = () => {
     const updatedList = [...lists];
-
-    return updatedList[id].todos[todo.id];
+    return updatedList?.[id]?.todos?.[todo.id] ?? null;
   };
 
+  // Create reactive isTodoDone variable to use it in the InputCheckbox component
+  const [isTodoCompleted, setIsTodoCompleted] = useState(
+    getTodo() ? getTodo().isCompleted : false
+  );
+
+  // The function of toggling the todo details menu
+  const toggleTodoDetails = () => {
+    setIsDetailsOpen(true);
+    setSelectedTodo(getTodo());
+    setFavoriteTodosId(id);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(userDocRef, (doc: any) => {
+      const updatedLists = [...doc.data().lists];
+      const updatedTodo = updatedLists[id].todos.find(
+        (t: any) => t.id === todo.id
+      );
+
+      if (updatedTodo) {
+        setIsTodoCompleted(updatedTodo.isCompleted);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [id, todo.id, userDocRef]);
+
+  useEffect(() => {
+    const updateTodoCompletedStatus = async () => {
+      const updatedLists = [...lists];
+      updatedLists[id].todos[todo.id].isCompleted = isTodoCompleted;
+
+      await updateDoc(userDocRef, {
+        lists: updatedLists,
+      });
+    };
+
+    updateTodoCompletedStatus();
+  }, [isTodoCompleted, id, todo.id, userDocRef]);
+
   return (
-    <div className={styles.todo} onClick={toggleTodoDetails}>
-      <p> {todo.title}</p>
-    </div>
+    <motion.div
+      className={styles.todo}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.2 }}
+      layout
+    >
+      <InputCheckbox
+        onChange={(event) => setIsTodoCompleted(event.target.checked)}
+        checked={isTodoCompleted}
+      />
+      <p onClick={toggleTodoDetails}>{todo.title}</p>
+    </motion.div>
   );
 }
--- src/features/todos/todo/types.d.ts
+++ src/features/todos/todo/types.d.ts
@@ -1,6 +1,11 @@
 type TodoProps = {
   id?: number;
-  todo: { id: number; title: string; isFavorite: boolean };
+  todo: {
+    id: number;
+    title: string;
+    isFavorite: boolean;
+    isCompleted: boolean;
+  };
   setIsDetailsOpen: React.Dispatch<React.SetStateAction<boolean>>;
   setSelectedTodo: React.Dispatch<React.SetStateAction<undefined>>;
   setFavoriteTodosId?: React.Dispatch<React.SetStateAction<number>>;
