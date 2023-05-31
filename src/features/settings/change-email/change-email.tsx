dc27b0079a3f57b64cab8618faf9915c9fb0ca29
Ozgur Oguzturk <ozguroguzturk@gmail.com>
2023-05-31 20:23:03 +0300

    Create change email setting feature

---
+++ src/features/settings/change-email/change-email.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signOut, updateEmail } from "firebase/auth";
+import { auth } from "src/utils/firebase-config";
+import BaseCard from "src/components/base-card";
+import InputEmail from "src/components/input-email";
+import Button from "src/components/button";
+import styles from "./change-email.module.scss";
+
+export default function ChangeEmail() {
+  // Create a reactive email variable to use it in the InputEmail component
+  const [email, setEmail] = useState(auth.currentUser!.email);
+
+  // Create a navigate variable using useNavigate hook to navigate between pages
+  const navigate = useNavigate();
+
+  // Function to change user's e-mail account
+  const changeEmail = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+
+    if (email != auth.currentUser?.email) {
+      updateEmail(auth.currentUser!, email!)
+        .then(() => {
+          signOut(auth)
+            .then(() => {
+              navigate("/sign-in");
+            })
+            .catch((error) => {
+              console.error(error.code, error.message);
+            });
+        })
+        .catch((error) => {
+          if (error.code == "auth/requires-recent-login") {
+            alert("You must sign in again before changing your E-Mail!");
+            navigate("/sign-in");
+          }
+        });
+    }
+  };
+  return (
+    <BaseCard>
+      <form className={styles["change-email"]} onSubmit={changeEmail}>
+        <p className={styles["change-email__title"]}>Change E-Mail</p>
+
+        <InputEmail
+          placeholder="Enter A New E-Mail..."
+          onChange={(event) => setEmail(event.target.value)}
+          value={email!}
+        />
+        <div className={styles["change-email__button"]}>
+          <Button type="submit">Save</Button>
+        </div>
+      </form>
+    </BaseCard>
+  );
+}
