113a6b653860e1afbd140d94341870233fe7e452
Özgür Oğuztürk <ozguroguzturk@gmail.com>
2023-06-14 02:47:51 +0300

    Add className prop

--- src/components/base-card/base-card.tsx
+++ src/components/base-card/base-card.tsx
@@ -1,11 +1,14 @@
 import styles from "./base-card.module.scss";
 
 type BaseCardProps = {
+  className?: string;
   children?: React.ReactNode;
 };
 
-const BaseCard = ({ children }: BaseCardProps) => {
-  return <div className={styles["base-card"]}>{children}</div>;
+const BaseCard = ({ className, children }: BaseCardProps) => {
+  return (
+    <div className={`${styles["base-card"]} ${className}`}>{children}</div>
+  );
 };
 
 export default BaseCard;
