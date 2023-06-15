9f998c54a3a20452214fcd94d101676f51cc2e51
Özgür Oğuztürk <ozguroguzturk@gmail.com>
2023-06-15 20:08:42 +0300

    Update firebase config

--- src/utils/firebase-config.ts
+++ src/utils/firebase-config.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJLqn45nh8EsCU5-2Be1fLNc2qZVqrVmw",
-  authDomain: "react-todo-1f2ec.firebaseapp.com",
-  projectId: "react-todo-1f2ec",
-  storageBucket: "react-todo-1f2ec.appspot.com",
-  messagingSenderId: "223716289488",
-  appId: "1:223716289488:web:071188b0be84f9d64727d5",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
